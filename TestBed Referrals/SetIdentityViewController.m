98888558c63fc90b2c10874e904433c68acb1b79
parthkalavadia <parth@branch.io>
2018-01-03 13:08:39 -0800

    textField view fixed when keyboard appears

--- TestBed Referrals/SetIdentityViewController.m
+++ TestBed Referrals/SetIdentityViewController.m
@@ -10,30 +10,50 @@
 #import "Branch.h"
 #import "AlertHelper.h"
 @interface SetIdentityViewController ()<UITextFieldDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *userIdTF;
-@property (weak, nonatomic) IBOutlet UIButton *userIdBtn;
-@property (weak, nonatomic) IBOutlet UITextView *infoTextField;
-@property (strong, nonatomic) IBOutlet UIView *deepLinkBanner;
-@property (strong, nonatomic) IBOutlet UIImageView *guideBannerImageView;
+@property (weak, nonatomic) IBOutlet    UITextField *userIdTF;
+@property (weak, nonatomic) IBOutlet    UIButton *userIdBtn;
+@property (weak, nonatomic) IBOutlet    UITextView *infoTextField;
+@property (strong, nonatomic) IBOutlet  UIImageView *guideBannerImageView;
+@property (strong, nonatomic) IBOutlet  UILabel *deepLinkBannerLbl;
+@property (strong, nonatomic) IBOutlet  UIScrollView *scrollView;
+@property (assign, nonatomic) BOOL      keyboardIsShown;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *scrollViewTopSpacingConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *deepLinkBannerHeightConstaint;
 
 @end
 
 @implementation SetIdentityViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    if (self.isReferrer) {
-        self.deepLinkBanner.hidden = YES;
-    }
-    else {
-        self.deepLinkBanner.hidden = NO;
+    if (!self.isReferrer) {
         self.infoTextField.text = [NSString stringWithFormat:@"Before we go ahead and redeem the credits, we need to set unique identifier which could be an email id, phone number, UUID etc.\n\nThis identifier would uniquely identify the referred user."];
         self.guideBannerImageView.image = [UIImage imageNamed:@"setId_referred"];
     }
     self.userIdBtn.layer.cornerRadius = 5;
     self.userIdBtn.clipsToBounds = YES;
     self.userIdTF.delegate = self;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:self.view.window];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:self.view.window];
+    _keyboardIsShown = NO;
+
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (!self.isReferrer) {
+        self.deepLinkBannerHeightConstaint.constant = 40;
+        [UIView animateWithDuration:0.3 animations:^{
+            [self.view layoutIfNeeded];
+        }];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -62,6 +82,7 @@
                 else {
                     [self performSegueWithIdentifier:@"referrerUserSegue" sender:self];
                 }
+                [self resignKeyboard:nil];
             }
             else {
                 //Alert Couldn't set identity
@@ -84,5 +105,28 @@
 - (void)configureControlWithData:(NSDictionary *)data {
     self.isReferrer = NO;
 }
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+    self.scrollViewTopSpacingConstraint.constant = 0;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    _keyboardIsShown = NO;
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    if (_keyboardIsShown) {
+        return;
+    }
+    
+    self.scrollViewTopSpacingConstraint.constant = -180;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    _keyboardIsShown = YES;
+}
+
 
 @end
